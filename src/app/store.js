405ca2393b11df951d9e70c7861615025f6ea969
Charles Young <cbyoung415@gmail.com>
2022-12-06 09:37:01 -0800

    add topicsReducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from "../features/topics/topicsSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    topics: topicsReducer,
+  },
 });
