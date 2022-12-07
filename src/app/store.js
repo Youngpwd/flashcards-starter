bd972bc044dc430f0af986ad5715c7f78ac4c982
Charles Young <cbyoung415@gmail.com>
2022-12-07 11:05:31 -0800

    add quizzesReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from "../features/topics/topicsSlice";
+import quizzesReducer from "../features/quizzes/quizzesSlice";
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
+    quizzes: quizzesReducer,
   },
 });
